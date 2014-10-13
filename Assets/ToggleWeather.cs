c3dbac279dfef78179f1235029bb52ff7c8811e0
Vongkhamsay <jvongkhamsay@hotmail.com>
2014-10-13 13:08:26 -0500

    Added rain and collision

---
+++ Assets/ToggleWeather.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleWeather : MonoBehaviour {
+
+
+
+	bool rain = false;
+	// Use this for initialization
+	void Start () {
+
+	
+			
+	
+				
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (Input.GetKey (KeyCode.UpArrow) ){
+			gameObject.SetActive (false);
+			AudioListener.pause = true;
+			rain = false;
+				}
+
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			gameObject.SetActive (true);
+			AudioListener.pause = false;
+			rain = true;
+		}
+						
+
+
+			
+			
+				
+
+	
+	}
+}
